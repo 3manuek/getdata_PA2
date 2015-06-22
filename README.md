ef0ae134dae74e1f8e33932e72da092095133fc0
Emanuel Calvo <3manuek@esdebian.org>
2015-06-21 21:45:13 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Programming assignment 2 - Cleaning Data (coursera)
+
+
+run_analysis.R does:
+
+    - Merges the training and the test sets to create one data set.
+    - Extracts only the measurements on the mean and standard deviation for each measurement. 
+    - Uses descriptive activity names to name the activities in the data set
+    - Appropriately labels the data set with descriptive variable names. 
+    - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+# Links
+
+Article about wereable technology:
+http://www.insideactivitytracking.com/data-science-activity-tracking-and-the-battle-for-the-worlds-top-sports-brand/
+
+Data Set descrition (Samsung Galaxy S) 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Data Set:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(dplyr)
+
+# Configuration section (paths)
+HAR_dataset <- "UCI HAR Dataset"
+setwd(HAR_dataset)
+working_dirs <- c("test/", "train/")
+test_dir <- file.path(working_dirs[1], "/")
+train_dir <-file.path(working_dirs[2], "/")
+
+# Functions
+# Step 4 function
+trim <- function (x) gsub("\\s+|\\(|\\)|\\-|\\,", "", x)
+
+# ... and Step 4 ;)
+colnames <- trim(read.csv("features.txt", row.names = NULL, header = FALSE, sep =" ")[,2])
+
+
+testDataSet <- read.table(file.path(test_dir, "X_test.txt"), row.names = NULL, header = FALSE,
+                    col.names = colnames)
+
+# Step 1
+# rbind Train with the testDataSet
+dataset <- rbind(testDataSet, read.table(file.path(train_dir, "X_train.txt"), 
+                                         row.names = NULL, header = FALSE,
+                                         col.names = colnames))
+
+datasetmean  <- select(dataset, ends_with("mean"))
+datasetstd   <- select(dataset, ends_with("std"))
+
+# Step 2 
+step2 <- cbind(datasetmean,datasetstd)
+
+testActivities   <- read.csv(file.path(test_dir, "y_test.txt"), header= FALSE , col.names = "Activity")
+trainActivities  <- read.csv(file.path(train_dir, "y_train.txt"), header= FALSE , col.names = "Activity")
+allActivities    <- rbind(testActivities,trainActivities)
+labelss <- read.csv("activity_labels.txt", header = FALSE, row.names = NULL, sep = " ", col.names = c("Activity", "Label"))
+
+# Step 3
+step3 <- mutate(step2, Activity = (left_join(allActivities,labelss, by="Activity")[,2]) )
+
+
+
+
